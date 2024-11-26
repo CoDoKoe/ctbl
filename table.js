0ca7f9b70d1da9ad31fb61e2d6945dd31b22e03a
Cornelius Kömpel <cdk@collus.de>
2024-11-26 18:44:27 +0100

    Suche

--- table.js
+++ table.js
@@ -7,7 +7,14 @@
 let ctbl = {
     ascendingSymbol: '\u25B2',
     descendingSymbol: '\u25BC',
+    timeout: 500,
+    /**
+     * OnClick Event für die Sortierung
+     * @param {object} event Klick Event
+     */
     sortWait(event) {
+        event.stopPropagation();
+        event.preventDefault();
 
         event.target.style.cursor = 'wait';
         document.body.style.cursor = 'wait';
@@ -15,8 +22,12 @@ let ctbl = {
             ctbl.sort(event);
             event.target.style.cursor = 'pointer';
             document.body.style.cursor = 'default';
-        }, 1000);
+        }, ctbl.timeout);
     },
+    /**
+     * Die eigentliche Sortierung erfolget in der Funktin sortTable, hier absteigend oder aufsteigend und anschließendes Markup
+     * @param {object} event Click Event
+     */
     sort(event) {
 
         let th = event.target;
@@ -63,6 +74,12 @@ let ctbl = {
         th.style.cursor = 'pointer';
 
     },
+    /**
+     * Die eigentliche Sortierung der Tabelle
+     * @param {object} table Die Tabelle
+     * @param {object} col die Spalte
+     * @param {integer} asc Sortierung 0 absteigend, 1 aufsteigend
+     */
     sortTable: function (table, col, asc) {
 
         let rows = Array.from(table.querySelectorAll("tbody tr"));
@@ -94,14 +111,60 @@ let ctbl = {
         // Füge die sortierten Zeilen wieder ein
         tBody.append(...rows);
     },
+    filter: function (event) {
+        event.stopPropagation();
+        event.preventDefault();
+        let table = event.target.closest('table');
+        let col = event.target.cellIndex;
+        ctbl.suchListe(table, col);
+        ctbl.dialog(table, col);
+    },
+
+    suchListe: function (table, col) {
+        const spalte = col + 1;
+        let liste = [];
+        let liste2 = [];
+        const tds = table.querySelectorAll(`tbody tr td:nth-child(${spalte})`);
+        tds.forEach(el => {
+            liste.push(el.textContent);
+        })
+        liste = [...new Set(liste)].sort((a, b) => a.localeCompare(b, "de", { numeric: true }));
+
+        //prüfen, ob datalist schon vorhanden ist, wenn nein anlegen
+        let dataliste = document.querySelector('datalist#suchListe');
+        if (!dataliste) {
+            dataliste = document.createElement('datalist');
+            dataliste.id = "suchListe";
+            let body = document.querySelector('body');
+            body.appendChild(dataliste);
+        }
+        //datalist leeren
+        dataliste.innerHTML = "";
+
+        //datalist füllen
+        liste.forEach((el) => {
+            let option = document.createElement('option');
+            option.innerHTML = el;
+            dataliste.appendChild(option);
+        });
+    },
+
     filterTable: function (table, col, filter) {
+        console.log(table, col, filter);
+
+
+        //let filter = "InRechnung";
         let rows = Array.from(table.querySelectorAll("tbody tr"));
         let tBody = table.querySelector("tbody");
 
         // Filtere die Zeilen
         rows = rows.filter(row => {
             let cell = row.querySelectorAll("td")[col];
-            return cell.textContent.trim().toLowerCase().includes(filter.toLowerCase());
+
+            // Volltextsuche
+            let searchText = filter.toLowerCase();
+            let cellText = cell.textContent.trim().toLowerCase();
+            return cellText.includes(searchText);
         });
 
 
@@ -112,5 +175,25 @@ let ctbl = {
 
         // Füge die gefilterten Zeilen wieder ein
         tBody.append(...rows);
+    },
+    dialog: function (table, col) {
+        let dialog = document.createElement('dialog');
+        dialog.innerHTML = `Filer: <input type="text" id="filter" name="filter" value="" list="suchListe">`;
+        dialog.innerHTML += `<button id="filterButton">Filtern</button>`;
+        dialog.innerHTML += `<button id="closeButton">Schließen</button>`;
+        dialog.querySelector('#filterButton').addEventListener('click', function () {
+            ctbl.filterTable(table, col, dialog.querySelector('#filter').value);
+            dialog.close();
+        });
+
+        dialog.querySelector('#closeButton').addEventListener('click', function () {
+            dialog.close();
+        });
+        dialog.addEventListener('close', function () {
+            dialog.remove();
+        });
+
+        document.body.appendChild(dialog);
+        dialog.showModal();
     }
 }
